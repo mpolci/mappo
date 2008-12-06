f2f2d70daa9133096e14811f88937e3ed48206e9
mpolci <mpolci@18abc9ad-5302-8325-3302-5372852159d8>
2008-12-06 07:21:41 +0000

    Sistemato detect dell'altezza del font anche in piattaforma windows impostando la corretta dll in dllimport

--- trunk/MapControl/Tools.cs
+++ trunk/MapControl/Tools.cs
@@ -7,7 +7,6 @@ namespace MapsLibrary
     class Tools
     {
 
-#if PocketPC
         public static TEXTMETRICS GetTextMetrics(System.Drawing.Font font)
         {
             IntPtr hDC = GetDC(IntPtr.Zero); //Screen DC
@@ -20,28 +19,35 @@ namespace MapsLibrary
             ReleaseDC(IntPtr.Zero, hDC);
             return tm;
         }
+#if PocketPC || Smartphone || WindowsCE
+        const string dll_dc="coredll";
+        const string dll_obj="coredll";
+#else
+        const string dll_dc="user32";
+        const string dll_obj = "gdi32";
+#endif
 
-        [DllImport("coredll")]
+        [DllImport(dll_dc)]
         private extern static IntPtr GetDC(IntPtr hWnd);
 
-        [DllImport("coredll")]
+        [DllImport(dll_dc)]
         private extern static IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
 
-        [DllImport("coredll")]
+        [DllImport(dll_obj)]
         private extern static IntPtr SelectObject(IntPtr hDC, IntPtr hObj);
 
-        [DllImport("coredll")]
+        [DllImport(dll_obj)]
         private extern static IntPtr DeleteObject(IntPtr hObject);
 
-        [DllImport("coredll", CharSet = CharSet.Unicode)]
+        [DllImport(dll_obj, CharSet = CharSet.Unicode)]
         private extern static int GetTextMetrics(IntPtr hDC, ref TEXTMETRICS tm);
-#else
-        public static TEXTMETRICS GetTextMetrics(System.Drawing.Font font)
-        {
-            TEXTMETRICS tm = new TEXTMETRICS();
-            return tm;
-        }
-#endif
+//#else
+//        public static TEXTMETRICS GetTextMetrics(System.Drawing.Font font)
+//        {
+//            TEXTMETRICS tm = new TEXTMETRICS();
+//            return tm;
+//        }
+//#endif
     }
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
