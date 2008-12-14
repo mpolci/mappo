a2cfaa54607b1bed5ee9510aa5b3d9fc300fd888
mpolci <mpolci@18abc9ad-5302-8325-3302-5372852159d8>
2008-12-14 10:23:12 +0000

    Creata classe CachedTilesMapDL derivata da CachedTilesMap per caricare i tile da disco in background.

--- trunk/MapControl/ReadAheadCachedTilesMap.cs
+++ trunk/MapControl/ReadAheadCachedTilesMap.cs
@@ -32,7 +32,7 @@ namespace MapsLibrary
             threadrun = true;
             thrReadAhead = new Thread(new ThreadStart(this.RedAheadThreadProc));
             thrReadAhead.Priority = ThreadPriority.BelowNormal;
-            thrReadAhead.Name = "Tiles preloader";
+            thrReadAhead.Name = "Tiles predownloader";
             thrReadAhead.Start();
         }
 
@@ -92,6 +92,7 @@ namespace MapsLibrary
             threadrun = false;
             jobEvent.Set();
             thrReadAhead.Join();
+            base.Dispose();
         }
 
         #endregion
