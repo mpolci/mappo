5c8e07f45ce381f288c93b0af7a5007c305e7fb0
mpolci <mpolci@18abc9ad-5302-8325-3302-5372852159d8>
2009-10-25 23:31:46 +0000

    Aggiunta class LayerSpatialObjects

---
+++ trunk/MapControl/LayerSpatialObjects.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace MapsLibrary
+{
+    public abstract class DrawableSpatialObjects : SpatialObject
+    {
+        public abstract void Draw(System.Drawing.Graphics dst, PxCoordinates corner, uint zoom, MercatorProjectionMapSystem mapsystem);
+    }
+
+    public class LayerSpatialObjects : IMap
+    {
+
+ 
+        protected SpatialObjectCollection objects;
+        protected MercatorProjectionMapSystem mapsys;
+        protected static readonly double ln2 = Math.Log(2);
+
+        public event MapChangedEventHandler MapChanged;
+
+        public MercatorProjectionMapSystem mapsystem
+        {
+            get { return mapsys; }
+        }
+
+        public LayerSpatialObjects(MercatorProjectionMapSystem mapsystem)
+        {
+            mapsys = mapsystem;
+            objects = new SpatialObjectCollection();
+        }
+
+        public virtual void addObject(DrawableSpatialObjects o)
+        {
+            objects.AddObject(o);
+
+            if (MapChanged != null)
+                MapChanged(this, o.ContainingArea);
+        }
+
+        /// <summary>
+        /// Elimina tutti i punti dal layer
+        /// </summary>
+        public virtual void clear()
+        {
+            ProjectedGeoArea area = objects.ContainigArea;
+            objects = new SpatialObjectCollection();
+            if (MapChanged != null)
+                MapChanged(this, area);
+        }
+
+        public virtual void drawImageMapAt(ProjectedGeoPoint map_center, uint zoom, ProjectedGeoArea area, Graphics dst, Point delta)
+        {
+            // calcola la profondità massima per la visita dell'albero
+            PxCoordinates pxcsize = mapsys.PointToPx(area.pMax - area.pMin, zoom) + new PxCoordinates(1, 1); ;
+            int depth = calcMaxDepth(area, new Size((int)pxcsize.xpx, (int)pxcsize.ypx));
+            PxCoordinates pxGraphCorner = mapsys.PointToPx(area.pMin, zoom);
+            pxGraphCorner.xpx -= delta.X;
+            pxGraphCorner.ypx -= delta.Y;
+
+            foreach (DrawableSpatialObjects dso in objects.Iterate(area, depth))
+            {
+                dso.Draw(dst, pxGraphCorner, zoom, this.mapsys);
+            }
+        }
+
+        /// <summary>
+        /// Calcola la profondità massima necessaria per la visita dell'albero, supponendo di avere 
+        /// una certa area da rappresentare su un'immagine di una certa grandezza in pixel.
+        /// </summary>
+        /// <param name="drawarea">Area da rappresentare.</param>
+        /// <param name="wsize">Dimensione in pixel dell'immagine rappresentante l'area indicata.</param>
+        /// <returns></returns>
+        protected int calcMaxDepth(ProjectedGeoArea drawarea, Size wsize)
+        {
+            // calcola la profondità massima per la visita dell'albero            
+            double dContaining = (double)Math.Max(objects.ContainigArea.height, objects.ContainigArea.width),
+                   dWindow = (double)Math.Max(drawarea.height, drawarea.width);
+            // int depth = (int)Math.Ceiling(Math.Log(dContaining / dWindow) / ln2) + (int)Math.Ceiling(Math.Log(size.Width) / ln2); 
+            int depth = (int)Math.Ceiling(Math.Log(dContaining / dWindow) / ln2 + Math.Log(Math.Max(wsize.Width, wsize.Height)) / ln2);
+            // a quanto pare se dContaining è 0 alla fine il risultato di depth è 0, anche se (int) di -infinito non è ovvio che dia 0
+            return depth;
+        }
+    }
+}
